64e17b18051c3c8d87b2bc08d7bd81242115c793
dlskrud815 <dlskrud815@gmail.com>
2024-04-23 17:25:55 +0900

    add using namespace std

--- Exercise6.cpp
+++ Exercise6.cpp
@@ -2,25 +2,28 @@
 // 1~9단까지 모두 출력해보기
 
 #include <iostream>
+using namespace std;
 
 int main() 
 {
-	std::cout << "5단 출력\n";
+	cout << "5단 출력\n";
 
 	for (int i = 1; i < 10; i++)
 	{
-		std::cout << "5 * " << i << " = " << 5 * i << std::endl;
+		cout << "5 * " << i << " = " << 5 * i << endl;
 	}
 
-	std::cout << "\n1~9단 출력\n";
+	cout << "\n1~9단 출력\n";
 
 	for (int j = 1; j < 10; j++)
 	{
-		std::cout << "------ " << j << "단 ------\n";
+		cout << "---- " << j << "단 ----\n";
+
 		for (int k = 1; k < 10; k++)
 		{
-			std::cout << j <<" * " << k << " = " << j * k << std::endl;
+			cout << j <<" * " << k << " = " << j * k << endl;
 		}
 	}
+
 	return 0;
 }
