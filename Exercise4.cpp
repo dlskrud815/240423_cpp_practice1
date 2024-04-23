3f138fa5f774ffcfe94f15aee87d86e813f574f0
dlskrud815 <dlskrud815@gmail.com>
2024-04-23 15:39:29 +0900

    change int to float

--- Exercise4.cpp
+++ Exercise4.cpp
@@ -5,7 +5,7 @@ int main()
 	//int num1, num2, operator1, result;
 	//char operator2;
 
-	int num1, num2, result;
+	float num1, num2, result;
 	char operator1;
 
 	std::cout << "두 개의 변수와 연산자를 입력 받아 계산할 수 있는 프로그램\n" 
@@ -13,7 +13,7 @@ int main()
 	std::cin >> num1;
 	std::cout << "두 번째 변수를 입력하세요 : ";
 	std::cin >> num2;
-	std::cout << "연산자를 입력하세요(+,-,*,/,%) : ";
+	std::cout << "연산자를 입력하세요(+,-,*,/) : ";
 	//std::cout << "연산자를 입력하세요 (+:0, -: 1, *: 2, /: 3, %: 4) : ";
 	std::cin >> operator1;
 
@@ -32,9 +32,11 @@ int main()
 	case '/':
 		result = num1 / num2;
 		break;
-	case '%':
+	/*
+	 	case '%':
 		result = num1 % num2;
 		break;
+	*/
 	default:
 		std::cout << "잘못된 연산자입니다.\n";
 	}
