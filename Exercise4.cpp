b472ea6b9a127b73d2f1020915f962915ef7b735
dlskrud815 <dlskrud815@gmail.com>
2024-04-23 15:33:41 +0900

    remove while

--- Exercise4.cpp
+++ Exercise4.cpp
@@ -11,69 +11,66 @@ int main()
 	std::cout << "두 개의 변수와 연산자를 입력 받아 계산할 수 있는 프로그램\n" 
 		<< std::endl << "첫 번째 변수를 입력하세요 : ";
 	std::cin >> num1;
+	std::cout << "두 번째 변수를 입력하세요 : ";
+	std::cin >> num2;
+	std::cout << "연산자를 입력하세요(+,-,*,/,%) : ";
+	//std::cout << "연산자를 입력하세요 (+:0, -: 1, *: 2, /: 3, %: 4) : ";
+	std::cin >> operator1;
 
-	while (num1 != -1) {
-		std::cout << "두 번째 변수를 입력하세요 : ";
-		std::cin >> num2;
-		std::cout << "연산자를 입력하세요(+,-,*,/,%) : ";
-		//std::cout << "연산자를 입력하세요 (+:0, -: 1, *: 2, /: 3, %: 4) : ";
-		std::cin >> operator1;
-
-
-		switch (operator1)
-		{
-		case '+':
-			result = num1 + num2;
-			break;
-		case '-':
-			result = num1 - num2;
-			break;
-		case '*':
-			result = num1 * num2;
-			break;
-		case '/':
-			result = num1 / num2;
-			break;
-		case '%':
-			result = num1 % num2;
-			break;
-		default:
-			std::cout << "잘못된 연산자입니다.\n";
-		}
-
-		if (result)
-		{
-			std::cout << num1 << operator1 << num2 << "=" << result << std::endl;
-		}
-
-		/*
-		switch (operator1)
-		{
-		case 0:
-			operator2 = '+';
-			result = num1 + num2;
-			break;
-		case 1:
-			operator2 = '-';
-			result = num1 - num2;
-			break;
-		case 2:
-			operator2 = '*';
-			result = num1 * num2;
-			break;
-		case 3:
-			operator2 = '/';
-			result = num1 / num2;
-			break;
-		case 4:
-			operator2 = '%';
-			result = num1 % num2;
-			break;
-		}
-
-		std::cout << num1 << operator2 << num2 << "=" << result << std::endl;
-		*/
+	
+	switch (operator1)
+	{
+	case '+':
+		result = num1 + num2;
+		break;
+	case '-':
+		result = num1 - num2;
+		break;
+	case '*':
+		result = num1 * num2;
+		break;
+	case '/':
+		result = num1 / num2;
+		break;
+	case '%':
+		result = num1 % num2;
+		break;
+	default:
+		std::cout << "잘못된 연산자입니다.\n";
 	}
+
+	if (result)
+	{
+		std::cout << num1 << operator1 << num2 << "=" << result << std::endl;
+	}
+
+	/*
+	switch (operator1)
+	{
+	case 0:
+		operator2 = '+';
+		result = num1 + num2;
+		break;
+	case 1:
+		operator2 = '-';
+		result = num1 - num2;
+		break;
+	case 2:
+		operator2 = '*';
+		result = num1 * num2;
+		break;
+	case 3:
+		operator2 = '/';
+		result = num1 / num2;
+		break;
+	case 4:
+		operator2 = '%';
+		result = num1 % num2;
+		break;
+	}
+
+	std::cout << num1 << operator2 << num2 << "=" << result << std::endl;
+	*/
 
 	return 0;
 }
