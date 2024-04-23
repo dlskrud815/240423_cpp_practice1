0140f2f8469df2dc58b05a461c6220b55ac57ae7
dlskrud815 <dlskrud815@gmail.com>
2024-04-23 15:50:25 +0900

    add enter

--- Exercise5.cpp
+++ Exercise5.cpp
@@ -6,7 +6,8 @@ int main() {
 	std::cout << "숫자를 입력해주세요." << std::endl;
 	std::cin >> num;
 
-	num % 5 == 0 ? std::cout << num << "은(는) 5의 배수입니다.\n" : std::cout << num << "은(는) 5의 배수가 아니네요ㅜㅜ\n";
+	num % 5 == 0 ? std::cout << num << "은(는) 5의 배수입니다.\n" 
+		: std::cout << num << "은(는) 5의 배수가 아니네요ㅜㅜ\n";
 
 	return 0;
 }
