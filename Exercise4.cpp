4308c7ea0853b93bcdec3aba5d06b41ab70cda96
dlskrud815 <dlskrud815@gmail.com>
2024-04-23 15:01:11 +0900

    change operator2 data type

--- Exercise4.cpp
+++ Exercise4.cpp
@@ -3,7 +3,7 @@
 int main()
 {
 	int num1, num2, operator1, result;
-	std::string operator2;
+	char operator2;
 
 	std::cout << "두 개의 변수와 연산자를 입력 받아 계산할 수 있는 프로그램" 
 		<< std::endl << "첫 번째 변수를 입력하세요 : ";
