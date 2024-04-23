4194bc69ca45eb98bc25e9c1d1d979c7c995a6ed
dlskrud815 <dlskrud815@gmail.com>
2024-04-23 15:51:53 +0900

    add while

--- Exercise5.cpp
+++ Exercise5.cpp
@@ -3,11 +3,18 @@
 int main() {
 	int num;
 
-	std::cout << "숫자를 입력해주세요." << std::endl;
-	std::cin >> num;
+	while (1) {
+		std::cout << std::endl << "숫자를 입력해주세요." << std::endl;
+		std::cin >> num;
 
-	num % 5 == 0 ? std::cout << num << "은(는) 5의 배수입니다.\n" 
-		: std::cout << num << "은(는) 5의 배수가 아니네요ㅜㅜ\n";
+		if (num == -1) 
+		{
+			return 0;
+		}
+
+		num % 5 == 0 ? std::cout << num << "은(는) 5의 배수입니다.\n"
+			: std::cout << num << "은(는) 5의 배수가 아니네요ㅜㅜ\n";
+	}
 
 	return 0;
 }
