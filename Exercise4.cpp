73bc37902657d030c8ea92085fd6faef4e8c8ca0
dlskrud815 <dlskrud815@gmail.com>
2024-04-23 15:17:13 +0900

    change switch data type int to char

--- Exercise4.cpp
+++ Exercise4.cpp
@@ -2,18 +2,44 @@
 
 int main()
 {
-	int num1, num2, operator1, result;
-	char operator2;
+	//int num1, num2, operator1, result;
+	//char operator2;
+
+	int num1, num2, result;
+	char operator1;
 
 	std::cout << "두 개의 변수와 연산자를 입력 받아 계산할 수 있는 프로그램" 
 		<< std::endl << "첫 번째 변수를 입력하세요 : ";
 	std::cin >> num1;
 	std::cout << "두 번째 변수를 입력하세요 : ";
 	std::cin >> num2;
-	std::cout << "연산자를 입력하세요 (+:0, -: 1, *: 2, /: 3, %: 4) : ";
+	std::cout << "연산자를 입력하세요(+,-,*,/,%) : ";
+	//std::cout << "연산자를 입력하세요 (+:0, -: 1, *: 2, /: 3, %: 4) : ";
 	std::cin >> operator1;
 
+	
+	switch (operator1)
+	{
+	case '+':
+		result = num1 + num2;
+		break;
+	case '-':
+		result = num1 - num2;
+		break;
+	case '*':
+		result = num1 * num2;
+		break;
+	case '/':
+		result = num1 / num2;
+		break;
+	case '%':
+		result = num1 % num2;
+		break;
+	}
 
+	std::cout << num1 << operator1 << num2 << "=" << result << std::endl;
+
+	/*
 	switch (operator1)
 	{
 	case 0:
@@ -39,6 +65,7 @@ int main()
 	}
 
 	std::cout << num1 << operator2 << num2 << "=" << result << std::endl;
+	*/
 
 	return 0;
 }
